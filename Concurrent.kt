7017fd2b33662562d5eabde1e9953067dd239ffb
Archan Patkar <archanpatkar@gmail.com>
2018-05-03 02:41:13 +0530

    Update

---
+++ Concurrent.kt
@@ -0,0 +1,34 @@
+package com.archanpatkar.tachyon;
+
+import java.net.*;
+import java.io.*;
+import kotlin.concurrent.thread;
+
+class ConcurrentTachyon: Tachyon
+{
+  private var ss:ServerSocket;
+  private var port:Int;
+
+  public constructor(port: Int)
+  {
+    this.port = port;
+    this.ss = ServerSocket(port);
+  }
+
+  public infix override fun start(cb: (soc:Socket,nis: BufferedReader,nos: PrintWriter) -> Unit)
+  {
+    println("ConcurrentTachyon Booting up!");
+    println("ConcurrentTachyon waiting on PORT:$port");
+    while(true)
+    {
+      val soc = ss.accept();
+      println("Connection Established");
+      val nis = BufferedReader(InputStreamReader(soc.getInputStream()));
+      val nos = PrintWriter(BufferedWriter(OutputStreamWriter(soc.getOutputStream())), true);
+      thread {
+        cb(soc,nis,nos);
+      }
+    }
+    println("ConcurrentTachyon Shutting Down");
+  }
+}
