690bbbd9b8a873defc83cbb3361675b4df385c01
Archan Patkar <archanpatkar@gmail.com>
2018-05-04 11:33:18 +0530

    Update

--- Serial.kt
+++ Serial.kt
@@ -3,7 +3,7 @@ package com.archanpatkar.tachyon;
 import java.net.*;
 import java.io.*;
 
-class SerialTachyon: Tachyon
+public class SerialTachyon: Tachyon
 {
   private var ss:ServerSocket;
   private var port:Int;
