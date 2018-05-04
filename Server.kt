40d7425c76a5d195ac386dd04d06a1bdeec691d0
Archan Patkar <archanpatkar@gmail.com>
2018-05-04 11:33:34 +0530

    Update

--- Server.kt
+++ Server.kt
@@ -1,10 +1,10 @@
 import java.net.*;
 import java.io.*;
-import com.archanpatkar.tachyon.SerialTachyon;
+import com.archanpatkar.tachyon.ConcurrentTachyon;
 
 fun main(args: Array<String>)
 {
-  val st1 = SerialTachyon(8081,10);
+  val st1 = ConcurrentTachyon(8081);
   st1 start { soc,nis,nos ->
     nos.println("hello from the other side!")
   }
